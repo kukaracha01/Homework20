9479bf05cbaa591d850ef76d965caa229e462951
mu <semkadarov@gmail.com>
2024-10-08 22:33:45 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        outputOddNums();
+        System.out.println(nums);
+    }
+
+    public static void outputOddNums() {
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
     }
 }
